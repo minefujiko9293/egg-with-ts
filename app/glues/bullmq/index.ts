aac3a332f8e53b534f40945709bf804281b9f56f
MineFurjiko <536546731@qq.com>
2024-01-21 22:34:38 +0800

    2024-01-21 22:37:27
    2024-01-21 22:34:37

--- app/glues/bullmq/index.ts
+++ app/glues/bullmq/index.ts
@@ -24,31 +24,29 @@ export async function setup(app: Application) {
 async function install(app: Application, connection: bullmq.RedisOptions) {
   const queueDir = path.join(__dirname, 'queue');
   try {
-    const files = await fs.promises.readdir(queueDir);
+    const modules = Array.from(
+      new Set(
+        (await fs.promises.readdir(queueDir)).map((file) =>
+          path.join(queueDir, file.replace(/\.[^.]+$/, '')),
+        ),
+      ),
+    );
+
     await Promise.all(
-      files.map(async (file) => {
-        const filePath = path.join(queueDir, file);
-
-        // 检查文件是否是 TypeScript 文件
-        if (filePath.endsWith('.ts') && !filePath.endsWith('.d.ts')) {
-          // 动态导入并执行文件
-          try {
-            const module = await import(filePath.replace(/\.ts$/, ''));
-
-            const result = module.default && module.default(app, connection);
-            if (result) {
-              app.bullmq[module.queue_name] = {
-                queue: result.queue,
-                worker: result.worker,
-              };
-            }
-          } catch (error) {
-            console.error(`Error importing ${file}:`, error);
+      modules.map(async (_module) => {
+        // 动态导入并执行文件
+        try {
+          const module = await import(_module);
+          const result = module.default && module.default(app, connection);
+          if (result) {
+            app.bullmq[module.queue_name] = {
+              queue: result.queue,
+              worker: result.worker,
+            };
           }
+        } catch (error) {
+          console.error(`Error importing ${_module}:`, error);
         }
-
-        // 对于非 TypeScript 文件或声明文件，返回一个 resolved promise
-        return Promise.resolve();
       }),
     );
   } catch (err) {
